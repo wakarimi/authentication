c1d8bea4e0ada1cc09c34cb981e983e473f03057
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-09-01 17:31:01 +0300

    feat: Add initial migration

---
+++ internal/database/db.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"database/sql"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"log"
+)
+
+func ConnectDb(connectionString string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func RunMigrations(db *sql.DB, migrationsPath string) error {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://"+migrationsPath,
+		"postgres",
+		driver,
+	)
+	if err != nil {
+		return err
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+
+	log.Println("Migrations applied successfully!")
+	return nil
+}
---
+++ internal/database/migrations/1_init.down.sql
@@ -0,0 +1,4 @@
+DROP TABLE "tokens";
+DROP TABLE "user_roles";
+DROP TABLE "roles";
+DROP TABLE "users";
