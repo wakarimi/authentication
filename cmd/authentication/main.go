5ee9f9bbb37c28bceb2a9d700417a48000299b3c
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-09-01 17:31:24 +0300

    feat: Connect to the database and create schema

--- cmd/authentication/main.go
+++ cmd/authentication/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"authentication/internal/config"
+	"authentication/internal/database"
+	"log"
+)
+
+func main() {
+	cfg, err := config.LoadConfiguration()
+	if err != nil {
+		log.Fatal("Failed to load configuration: %v", err)
+	}
+
+	log.Println(cfg.DatabaseConnectionString)
+	db, err := database.ConnectDb(cfg.DatabaseConfiguration.DatabaseConnectionString)
+	if err != nil {
+		log.Fatal("Failed to connect to the database: %v", err)
+	}
+	defer db.Close()
+
+	err = database.RunMigrations(db, "./internal/database/migrations")
+	if err != nil {
+		log.Fatal("Failed to run migrations: %v", err)
+	}
+}
